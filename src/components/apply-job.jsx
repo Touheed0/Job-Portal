a0f79dc207670497b5b63d827c73d383bfb7f692
Touheed0 <tuheedahmad360@gmail.com>
2025-11-26 07:48:30 -0800

    Update File

--- src/components/apply-job.jsx
+++ src/components/apply-job.jsx
@@ -1,4 +1,4 @@
-import z, { any, file } from "zod";
+import z from "zod";
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
